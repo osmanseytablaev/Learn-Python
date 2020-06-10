32ba526a451ba28e91f5737c0c3bfbceb301334a
osmanseytablaev <osman.seytablaev@gmail.com>
2020-06-10 10:00:07 +0300

    Goroscop version 1

---
+++ goroscop_version1.py
@@ -0,0 +1,68 @@
+number1 = 1
+number2 = 2
+number3 = 3
+number4 = 4
+number5 = 5
+number6 = 6
+number7 = 7
+number8 = 8
+number9 = 9
+number10 = 10
+number11 = 11
+number12 = 12
+while True:
+    date = int(input('Введите дату рождения: '))
+    if date == number1:
+        print('Водолей')
+    if date == number2:
+        print('Рыбы')
+    if date == number3:
+        print('Овен')
+    if date == number4:
+        print('Телец')
+    if date == number5:
+        print('Близнецы')
+    if date == number6:
+        print('Рак')
+    if date == number7:
+        print('Лев')
+    if date == number8:
+        print('Дева')
+    if date == number9:
+        print('Весы')
+    if date == number10:
+        print('Скорпион')
+    if date == number11:
+        print('Стрелец')
+    if date == number12:
+        print('Козерог')
+    if date == 1:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))    
+    if date == 2:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 3:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 4:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 5:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 6:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 7:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 8:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 9:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 10:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 11:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date == 12:
+        date2 = str(input('Чтобы выйти нажмите exit чтобы продолжить нажмите continue: '))
+    if date2 == 'continue':
+        continue
+    if date2 == 'exit':
+        break
+    
+
