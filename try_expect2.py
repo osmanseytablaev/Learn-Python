eba02908c09fe82b3c66858f5bf0c8412e347d27
osmanseytablaev <osman.seytablaev@gmail.com>
2020-06-14 21:47:34 +0300

    Using try-expect variant 2

--- try_expect2.py
+++
@@ -1,8 +0,0 @@
-try:
-    text = input('Введите что-нибудь --> ')
-except EOFError:
-    print('Ну зачем вы сделали мне EOF?')
-except KeyboardInterrupt:
-    print('Вы отменили операцию.')
-else:
-    print('Вы ввели {0}'.format(text))
